df6d3c042e986fe48fce1c3617fbd994083c831b
KDragon <elkarouani@gmail.com>
2019-03-08 00:13:18 +0100

    Task: displaying user name on the home page

---
+++ assets/js/home.js
@@ -0,0 +1,24 @@
+// caching DOM
+const greeting = document.getElementById('greeting');
+const xml = new XMLHttpRequest();
+
+// helpers :
+const getXmlData = (xml) => {
+    let xmlData = xml.responseText;
+    if (xmlData) {
+        return (new DOMParser()).parseFromString(xml.responseText, 'text/xml');
+    }
+    return xmlData;
+}
+
+const extractUserNameFromXml = (xml) => {
+    xml.open('GET', 'database/users.xml', false);
+    xml.send();
+    
+    let user = getXmlData(xml).getElementsByTagName("user")[0];
+        
+    return user.getElementsByTagName("name")[0].firstChild.data;
+}
+
+// main : 
+greeting.innerHTML = "Welcome Back : <ion-icon name='person' style='margin-left: 10px;'></ion-icon> " + extractUserNameFromXml(xml);
