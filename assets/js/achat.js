5f17dddaee6201f15a174ce2d9749caca1bf1ae0
KDragon <elkarouani@gmail.com>
2019-03-11 15:16:45 +0100

    Task: adding origins xml file to the project

---
+++ assets/js/achat.js
@@ -0,0 +1,15 @@
+// caching DOM
+xml = new XMLHttpRequest();
+
+// methods : 
+getXmlData = (xml) => {
+    xmlData = xml.responseText;
+    if (xmlData) {
+        return (new DOMParser()).parseFromString(xml.responseText, 'text/xml');
+    }
+    return xmlData;
+};
+
+// main : 
+
+// events : 
