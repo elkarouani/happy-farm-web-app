1ff5b32b0abe1d5ae37ec2ee8b6c50f7f222e3c7
KDragon <elkarouani@gmail.com>
2019-03-12 11:35:04 +0100

    Task: adding veals groups xml file to the project

--- assets/js/achat.js
+++ assets/js/achat.js
@@ -10,6 +10,21 @@ getXmlData = (xml) => {
     return xmlData;
 };
 
+const extractOriginsFromXml = (xml) => {
+	xml.open('GET', 'database/origins.xml', false);
+	xml.send();
+    return getXmlData(xml).getElementsByTagName("origin");
+}
+
+const fillWithOrigins = (origins) => {
+	for (let i = 0; i < origins.length; i++) {
+		// origins[i].firstChild.data
+
+	}
+}
+
 // main : 
+fillWithAnimalsList(extractAnlimalsListFromXml(xml));
 // fillWithOrigins();
+
 // events : 
