6af8c6ff647a509dfb2741d42ed83289bf6ef766
KDragon <elkarouani@gmail.com>
2019-03-12 18:35:53 +0100

    Task: adding condition of non existing group for displaying in table on buy veals component

--- assets/js/achat.js
+++ assets/js/achat.js
@@ -22,27 +22,29 @@ const extractVealGroupsFromXml = (xml) => {
 
 const fillWithVealGroups = (groups) => {
 	for (let i = 0; i < groups.length; i++) {
-		let newLine = document.createElement("tr");
-		let marketCell = document.createElement("td");
-		let originCell = document.createElement("td");
-		let weightCell = document.createElement("td");
-		let ageCell = document.createElement("td");
-		let priceCell = document.createElement("td");
-		let quantityCell = document.createElement("td");
-		let choicePointerCell = document.createElement("td");
-		
-		newLine.id = i;
-		marketCell.innerHTML = groups[i].childNodes[1].firstChild.data;
-		originCell.innerHTML = groups[i].childNodes[3].firstChild.data;
-		weightCell.innerHTML = groups[i].childNodes[5].firstChild.data;
-		ageCell.innerHTML = groups[i].childNodes[7].firstChild.data;
-		priceCell.innerHTML = groups[i].childNodes[9].firstChild.data;
-		quantityCell.innerHTML = "<input class='form-control' type='number' style='width: 3rem; padding: 0.3rem .5rem;' value='1' min='1' max='"+groups[i].childNodes[11].firstChild.data+"'>";
-		quantityCell.classList.add('d-flex', 'justify-content-center');
-		choicePointerCell.innerHTML = `<input class="form-control" type="checkbox" name="choice">`;
+		if (parseInt(groups[i].childNodes[11].firstChild.data) != 0) {
+			let newLine = document.createElement("tr");
+			let marketCell = document.createElement("td");
+			let originCell = document.createElement("td");
+			let weightCell = document.createElement("td");
+			let ageCell = document.createElement("td");
+			let priceCell = document.createElement("td");
+			let quantityCell = document.createElement("td");
+			let choicePointerCell = document.createElement("td");
+			
+			newLine.id = i;
+			marketCell.innerHTML = groups[i].childNodes[1].firstChild.data;
+			originCell.innerHTML = groups[i].childNodes[3].firstChild.data;
+			weightCell.innerHTML = groups[i].childNodes[5].firstChild.data;
+			ageCell.innerHTML = groups[i].childNodes[7].firstChild.data;
+			priceCell.innerHTML = groups[i].childNodes[9].firstChild.data;
+			quantityCell.innerHTML = "<input class='form-control' type='number' style='width: 3rem; padding: 0.3rem .5rem;' value='1' min='1' max='"+groups[i].childNodes[11].firstChild.data+"'>";
+			quantityCell.classList.add('d-flex', 'justify-content-center');
+			choicePointerCell.innerHTML = `<input class="form-control" type="checkbox" name="choice">`;
 
-		newLine.append(marketCell, originCell, weightCell, ageCell, priceCell, quantityCell, choicePointerCell);
-		vealGroupsTable.appendChild(newLine);
+			newLine.append(marketCell, originCell, weightCell, ageCell, priceCell, quantityCell, choicePointerCell);
+			vealGroupsTable.appendChild(newLine);
+		}
 	}
 }
 
