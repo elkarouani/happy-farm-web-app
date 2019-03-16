0f7faefcd8ff00d2923eb8dd1f7c8a569bb48414
KDragon <elkarouani@gmail.com>
2019-03-16 13:27:39 +0100

    Task: fill stock component with medicines infos

--- assets/js/stock.js
+++ assets/js/stock.js
@@ -4,6 +4,9 @@ let foodQuantity = document.getElementById('foodQuantity');
 let foodQuantityInput = document.getElementById('foodQuantityInput');
 let foodTotal = document.getElementById('foodTotal');
 let buyFood = document.getElementById('buyFood');
+let medicencesSelection = document.getElementById("medicencesSelection");
+let medicencesTable = document.getElementById("medicencesTable");
+let medicencesTotal = document.getElementById("medicencesTotal");
 message = document.getElementById('message');
 // helpers :
 getXmlData = (xml) => {
@@ -24,6 +27,31 @@ const extractFoodInfo = (xml) => {
     let quantite = nourriture.getElementsByTagName("quantite")[0].firstChild.data;
     
     return new Array(prix, quantite);
+}
+
+const extractMedicencesInfo = () => {
+    xml = new XMLHttpRequest();
+    xml.open('GET', 'database/stock.xml', false);
+    xml.send();
+    
+    let medicaments = getXmlData(xml).getElementsByTagName("medicament");
+    
+    return medicaments;
+} 
+
+const extractMedicinePriceByTitle = (xml, title) => {
+    xml = new XMLHttpRequest();
+    xml.open('GET', 'database/stock.xml', false);
+    xml.send();
+    
+    let medicaments = getXmlData(xml).getElementsByTagName("medicament");
+    for (let i = 0 ; i < medicaments.length ; i++) {
+        if (medicaments[i].childNodes[1].firstChild.data == title) {
+            return medicaments[i].childNodes[3].firstChild.data;
+        }
+    }
+    
+    return medicaments;
 }
 
 const addFoodQuantity = (xml, quantity) => {
@@ -55,10 +83,29 @@ const decreaseUserBudget = (xml, total) => {
     xml.send("total="+parseInt(total)+"&action=decreaseBudget");
 };
 
+const fillWithMedicences = () => {
+    let medicaments = extractMedicencesInfo(xml);
+    for (let i = 0 ; i < medicaments.length ; i++) {
+        let option = document.createElement("option");
+        option.value = medicaments[i].childNodes[1].firstChild.data;
+        option.innerHTML = medicaments[i].childNodes[1].firstChild.data;
+        medicencesSelection.appendChild(option);
+
+        let newLine = document.createElement("tr");
+        let titleCell = document.createElement("td");
+        let quantityCell = document.createElement("td");
+        titleCell.innerHTML = medicaments[i].childNodes[1].firstChild.data;
+        quantityCell.innerHTML = medicaments[i].childNodes[5].firstChild.data
+        newLine.append(titleCell, quantityCell);
+        medicencesTable.appendChild(newLine);
+    }
+};
+
 // main : 
 foodQuantity.innerHTML = "Quantité : "+extractFoodInfo(xml)[1]+" Kg";
 foodQuantityInput.value = 1;
 foodTotal.innerHTML = "Total : "+extractFoodInfo(xml)[0];
+fillWithMedicences();
 
 // Event : 
 foodQuantityInput.addEventListener('change', (event) => {
@@ -68,4 +115,8 @@ foodQuantityInput.addEventListener('change', (event) => {
 buyFood.addEventListener('click', (event) => {
     addFoodQuantity(xml, foodQuantityInput.value);
     decreaseUserBudget(xml, foodTotal.innerHTML.substring(('Total : ').length - 1));
+});
+
+medicencesSelection.addEventListener('change', () => {
+    medicencesTotal.innerHTML = "Total : " + extractMedicinePriceByTitle(xml, medicencesSelection.value);
 });
