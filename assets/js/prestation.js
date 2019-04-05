cf32782cc04ac3decb75ab6e63257725ee4ce1b4
KDragon <elkarouani@gmail.com>
2019-04-05 13:17:42 +0100

    Task: deploy table with data on prestations component

---
+++ assets/js/prestation.js
@@ -0,0 +1,72 @@
+// caching DOM
+const vealGroupsTable = document.getElementById("myTable");
+
+message = document.getElementById('message');
+xml = new XMLHttpRequest();
+
+// methods : 
+getXmlData = (xml) => {
+    xmlData = xml.responseText;
+    if (xmlData) {
+        return (new DOMParser()).parseFromString(xml.responseText, 'text/xml');
+    }
+    return xmlData;
+};
+
+const extractVealsFromXml = (xml) => {
+	xml.open('GET', 'database/farm.xml', false);
+	xml.send();
+    return getXmlData(xml).getElementsByTagName("veal");
+}
+
+const getConsultationsData = (xml) => {
+	xml.open('GET', 'database/consultation.xml', false);
+	xml.send();
+    return getXmlData(xml).getElementsByTagName("consultation");
+	
+}
+
+const getMedicencesByVeal = (xml, reference) => {
+	let consultations = getConsultationsData(xml);
+	for(let i = 0 ; i < consultations.length ; i++){
+		if (consultations[i].childNodes[0].firstChild.data == reference) {
+			return consultations[i].childNodes[5].childNodes;
+		}
+	}
+}
+
+const fillTable = (veals) => {
+	for (let i = 0; i < veals.length; i++) {
+		if (veals[i].childNodes[9].firstChild.data == "malade") {
+			let medicences = getMedicencesByVeal(xml, veals[i].childNodes[0].firstChild.data);
+			
+			let newLine = document.createElement("tr");
+			let refCell = document.createElement("td");
+			let medicencesCell = document.createElement("td");
+			let quantityCell = document.createElement("td");
+			let actionCell = document.createElement("td");
+			let medicencesSelection = document.createElement("select");
+			medicencesSelection.setAttribute("class", "form-control");
+
+			for(let j = 0 ; j < medicences.length ; j++){
+				let option = document.createElement("option");
+				option.value = medicences[0].firstChild.data;
+				option.innerText = medicences[0].firstChild.data;
+				medicencesSelection.appendChild(option);
+			}
+
+			refCell.innerHTML = veals[i].childNodes[0].firstChild.data;
+			quantityCell.innerHTML = "<input class='form-control' type='number' value='' style='width: 10rem; padding: .3rem .5rem;'/>";
+			actionCell.innerHTML = "<button class='btn btn-primary'>Envoyer</button>";
+			
+			medicencesCell.appendChild(medicencesSelection);
+			newLine.append(refCell, medicencesCell, quantityCell, actionCell);
+			vealGroupsTable.appendChild(newLine);
+		}
+	}
+}
+
+// main : 
+fillTable(extractVealsFromXml(xml));
+
+// events : 
