5df95f469808a969d9c99d6e9145132f5fc5b2c4
KDragon <elkarouani@gmail.com>
2019-09-18 16:29:07 +0100

    Task: clarify filling table functionality & table filter event

--- assets/js/achat.js
+++ assets/js/achat.js
@@ -1,4 +1,4 @@
-import { getNedeedDom, getNedeedClass } from "./helper.js";
+import { getNedeedDom, getNedeedClass, setEventListener, DomElementValue } from "./helper.js";
 
 // caching DOM
 let filterInput = getNedeedDom('myInput');
@@ -7,42 +7,28 @@ let addVealButton = document.getElementById("addVeal");
 let message = document.getElementById('message');
 let xml = new XMLHttpRequest();
 
-// methods : 
-const getXmlData = (xml) => {
-    let xmlData = xml.responseText;
-    if (xmlData) {
-        return (new DOMParser()).parseFromString(xml.responseText, 'text/xml');
-    }
-    return xmlData;
-};
-
+// functionality #1
 const fillWithVealGroups = () => {
+	// classes needed :
 	let Veals = getNedeedClass('Veals');
 	
-	setTimeout( () => { 
+	setTimeout( () => {
+		// for each veal in the veals collection ... 
 		Veals.collection.forEach((veal, index) => {
+			// if the kind of the veal is exist then ...
 			if (veal.max > 0) {
-				let newLine = document.createElement("tr");
-				let marketCell = document.createElement("td");
-				let originCell = document.createElement("td");
-				let weightCell = document.createElement("td");
-				let ageCell = document.createElement("td");
-				let priceCell = document.createElement("td");
-				let quantityCell = document.createElement("td");
-				let choicePointerCell = document.createElement("td");
-
-				newLine.id = index;
-				marketCell.innerHTML = veal.market;
-				originCell.innerHTML = veal.origin;
-				weightCell.innerHTML = veal.poid;
-				ageCell.innerHTML = veal.age;
-				priceCell.innerHTML = veal.prix;
+				// insert its informations in the table
+				let row = getNedeedDom('myTable').insertRow(index);
+				row.setAttribute('id', index);				
+				row.insertCell(0).innerHTML = veal.market;
+				row.insertCell(1).innerHTML = veal.origin;
+				row.insertCell(2).innerHTML = veal.poid;
+				row.insertCell(3).innerHTML = veal.age;
+				row.insertCell(4).innerHTML = veal.prix;
+				let quantityCell = row.insertCell(5);
 				quantityCell.innerHTML = "<input class='form-control' type='number' style='width: 4rem; padding: .3rem .5rem;' value='1' min='1' max='"+veal.max+"'>";
 				quantityCell.classList.add('d-flex', 'justify-content-center');
-				choicePointerCell.innerHTML = `<input class="form-control" type="checkbox" name="choice">`;
-		
-				newLine.append(marketCell, originCell, weightCell, ageCell, priceCell, quantityCell, choicePointerCell);
-				vealGroupsTable.appendChild(newLine);
+				row.insertCell(6).innerHTML = `<input class="form-control" type="checkbox" name="choice">`;
 			}
 		});
 	}, 1000 );
@@ -111,23 +97,11 @@ const getAvailableCharge = (xml) => {
 }
 
 // main : 
-
-
 fillWithVealGroups();
 
 // events : 
-filterInput.addEventListener('keyup', (event) => {
-	let value = filterInput.value.toUpperCase();
-	let lines = vealGroupsTable.getElementsByTagName("tr");
-	for (let i = 0 ; i < lines.length ; i++){
-		origin = lines[i].getElementsByTagName('td')[1].firstChild.data;
-		if (origin.toUpperCase().indexOf(value) > -1) {
-	        lines[i].style.display = "";
-	    } else {
-	        lines[i].style.display = "none";
-	    }
-	}
-})
+// event 1# : table filter
+setEventListener('myInput', ['keyup'], (event) => { for (let line of getNedeedDom('myTable').getElementsByTagName("tr")) { line.style.display = (line.getElementsByTagName('td')[1].firstChild.data.toUpperCase().indexOf(DomElementValue('myInput').toUpperCase()) > -1) ? "" : "none"; } })
 
 async function purchaseOperations(xml, totalWeight, vealsToInsert, originsToUpdate, priceToGive) {
 	let availableCharge = await getAvailableCharge(xml);
